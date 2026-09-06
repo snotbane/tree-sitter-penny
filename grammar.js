7d0f6a7d2d509d768596d294357de5b8ab6b3b1e
Sebastian Weaver <27900656+snotbane@users.noreply.github.com>
2026-09-06 16:51:53 -0500

    .

--- grammar.js
+++ grammar.js
@@ -8,64 +8,68 @@
 // @ts-check
 
 export default grammar({
-  name: "penny",
+	name: "penny",
 
-  extras: ($) => [/\s/, $.comment],
+	extras: ($) => [/\s/, $.comment],
 
-  word: ($) => $.identifier,
+	word: ($) => $.identifier,
 
-  rules: {
-    source_file: ($) => repeat($._statement),
+	rules: {
+		source_file: ($) => repeat(choice($._statement)),
 
-    _statement: ($) =>
-      choice(
-        $._statement_path,
-        $.statement_label,
-        $.statement_print,
-        $.statement_return,
-      ),
+		_statement: ($) =>
+			choice(
+				$._statement_path,
+				$.statement_label,
+				$.statement_print,
+				$.statement_return,
+			),
 
-    _statement_path: ($) => choice($.identifier),
+		_statement_path: ($) => choice($.identifier),
 
-    statement_label: ($) => seq("label", $.identifier),
+		statement_label: ($) => seq($._keyword_label, $.identifier),
 
-    statement_print: ($) => seq("print", $.expression),
+		statement_print: ($) => seq($._keyword_print, $.expression),
 
-    statement_return: ($) =>
-      prec.left(2, seq("return", optional($.expression))),
+		statement_return: ($) =>
+			prec.left(2, seq($._keyword_return, optional($.expression))),
 
-    expression: ($) => choice($.number, $.identifier),
+		expression: ($) => choice($.number, $.identifier),
 
-    keyword: ($) =>
-      prec(
-        100,
-        choice(
-          "await",
-          "call",
-          "elif",
-          "else",
-          "exit",
-          "if",
-          "init",
-          "jump",
-          "label",
-          "let",
-          "match",
-          "menu",
-          "new",
-          "object",
-          "pass",
-          "print",
-          "return",
-          "suspend",
-          "var",
-        ),
-      ),
+		_keyword: ($) =>
+			prec(
+				100,
+				choice(
+					"await",
+					"call",
+					"elif",
+					"else",
+					"exit",
+					"if",
+					"init",
+					"jump",
+					$._keyword_label,
+					"let",
+					"match",
+					"menu",
+					"new",
+					"object",
+					"pass",
+					$._keyword_print,
+					$._keyword_return,
+					"suspend",
+					"var",
+				),
+			),
 
-    identifier: ($) => /[a-z_][a-z_0-9]*/i,
+		_keyword_label: ($) => alias("label", $.keyword),
+		_keyword_print: ($) => alias("print", $.keyword),
+		_keyword_return: ($) => alias("return", $.keyword),
 
-    number: ($) => choice(/\d+/, /\d+\.\d+/, /\.\d+/),
+		identifier: ($) => /[a-z_][a-z_0-9]*/i,
 
-    comment: ($) => seq("#", /.*/),
-  },
+		number: ($) => choice(/\d+/, /\d+\.\d+/, /\.\d+/),
+
+		comment: ($) => seq("#", /.*/),
+	},
 });
